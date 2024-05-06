57cb1a50a809026023a8631a888333ee0d83f115
sooshiance <s.katafract@gmail.com>
2024-05-06 18:43:21 +0330

    add store path

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path 
 
 from user import views as auth_views
+from store import views as store_view
 
 
 urlpatterns = [
+    # TODO : Authentication paths
     path('user/token/', auth_views.UserAccessAPIView.as_view()),
     path('user/otp/', auth_views.CheckOTPAPIView.as_view()),
     path('user/profile/', auth_views.ProfileAPIView.as_view()),
+
+    # TODO : Store paths
+    path('store/category/', store_view.CategoryAPIView.as_view()),
+    path('store/category/<int:pk>/', store_view.SingleCategoryAPIView.as_view()),
+    path('store/product/', store_view.ProductAPIView.as_view()),
+    path('store/product/<int:pk>/', store_view.SingleProductAPIView.as_view()),
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -11,3 +11,34 @@ class CategoryAPIView(generics.ListAPIView):
 
     permission_classes = [permissions.AllowAny]
     serializer_class = CategorySerializer
+
+
+class SingleCategoryAPIView(generics.RetrieveAPIView):
+    """"""
+
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CategorySerializer
+
+    def get_object(self):
+        pk = self.kwargs['pk']
+        c = Category.objects.get(pk=pk)
+        return c
+
+
+class ProductAPIView(generics.ListAPIView):
+    """"""
+
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductSerializer
+
+
+class SingleProductAPIView(generics.RetrieveAPIView):
+    """"""
+
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProductSerializer
+
+    def get_object(self):
+        pk = self.kwargs['pk']
+        c = Product.objects.get(pk=pk)
+        return c
